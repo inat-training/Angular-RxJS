faa3304e29e2e48c4beefaa94980b517919a6d93
Perrence Muzavazi <7643707+mperrence@users.noreply.github.com>
2022-02-08 08:03:53 +0200

    initialize project files

---
+++ APM/src/app/products/product-list-alt/product-list-alt.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { EMPTY } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { Product } from '../product';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'pm-product-list',
+  templateUrl: './product-list-alt.component.html'
+})
+export class ProductListAltComponent {
+  pageTitle = 'Products';
+  errorMessage = '';
+  selectedProductId: number;
+
+  products$ = this.productService.products$.pipe(
+    catchError(error => {
+      this.errorMessage = error;
+      return EMPTY;
+    })
+  )
+  constructor(private productService: ProductService) { }
+
+  onSelected(productId: number): void {
+    console.log('Not yet implemented');
+  }
+}
---
+++ APM/src/app/products/product-list.component.ts
@@ -0,0 +1,38 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+import { EMPTY, Observable, Subscription } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { Product } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductListComponent {
+  pageTitle = 'Product List';
+  errorMessage = '';
+  categories;
+
+  products$ = this.productService.products$
+  .pipe(
+    catchError(error => {
+      this.errorMessage = error;
+      return EMPTY;
+    })
+  )
+  sub: Subscription;
+
+  constructor(private productService: ProductService) { }
+
+
+  onAdd(): void {
+    console.log('Not yet implemented');
+  }
+
+  onSelected(categoryId: string): void {
+    console.log('Not yet implemented');
+  }
+}
